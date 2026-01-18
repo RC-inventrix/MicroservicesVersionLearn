14aa73669037fcec9e7a37e33282b1af05d113cf
RCN <ravindunishshankawork@gmail.com>
2026-01-18 17:38:12 +0530

    feat(discovery server): Added the discovery server and configured it

--- order service/src/main/java/com/programmingtechi/orderservice/service/OrderService.java
+++ order service/src/main/java/com/programmingtechi/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -35,8 +35,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponseArray= webClient.get()
-                .uri("http://localhost:8082/api/inventory")
+        InventoryResponse[] inventoryResponseArray= webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory")
 
                         .retrieve()
                                 .bodyToMono(InventoryResponse[].class)
