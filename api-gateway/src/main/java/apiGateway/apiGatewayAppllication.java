bf22bb78d05ca8e7d581d6ba861e180a44c2f8f7
RCN <ravindunishshankawork@gmail.com>
2026-01-21 23:18:30 +0530

    ms(api-gateway):Configured the API gate way successfully

---
+++ api-gateway/src/main/java/apiGateway/apiGatewayAppllication.java
@@ -0,0 +1,13 @@
+package apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class apiGatewayAppllication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(apiGatewayAppllication.class, args);
+    }
+
+}
